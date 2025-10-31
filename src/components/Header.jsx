b16533e0cdae6debeefa4df8d863c2b754ce0d85
grouchomarxc137-star <grouchomarxc137@gmail.com>
2025-10-31 22:33:05 +0100

    Added the header of the application

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "../style/header.css"
+
+function Header(){
+    return(
+        <div className="header-container">
+            <h1 className="header-title">
+                CV Generator
+            </h1>
+            <p className="header-description">
+                Create your professional resume
+            </p>
+        </div>
+    )
+}
+export default Header;
